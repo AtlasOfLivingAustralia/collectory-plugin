b358bca54c04880cccfc6f82567c1e4053166a99
Dave Martin <djtfmartin@users.noreply.github.com>
2015-04-05 13:15:49 +1000

    Bumped to 1.1-SNAPSHOT

--- CollectoryGrailsPlugin.groovy
+++ CollectoryGrailsPlugin.groovy
@@ -9,7 +9,7 @@ class CollectoryGrailsPlugin {
     def authenticateService
 
     // the plugin version
-    def version = "1.0"
+    def version = "1.1-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.3 > *"
     // resources that are excluded from plugin packaging
