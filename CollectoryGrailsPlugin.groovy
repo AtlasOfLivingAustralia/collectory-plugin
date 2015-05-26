307178ddd49b30cf287516649ed254cd11d873c2
Dave Martin <djtfmartin@users.noreply.github.com>
2015-05-26 15:21:58 +0100

    1.3.3

--- CollectoryGrailsPlugin.groovy
+++ CollectoryGrailsPlugin.groovy
@@ -9,7 +9,7 @@ class CollectoryGrailsPlugin {
     def authenticateService
 
     // the plugin version
-    def version = "1.3.2"
+    def version = "1.3.3"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.3 > *"
     // resources that are excluded from plugin packaging
