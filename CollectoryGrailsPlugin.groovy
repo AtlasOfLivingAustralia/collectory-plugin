2ddc45d15c28964fbede58063f2cecf1a687223d
Nick dos Remedios <nickdos@gmail.com>
2015-11-19 20:33:51 +1100

    Version 1.6-SNAPSHOT

--- CollectoryGrailsPlugin.groovy
+++ CollectoryGrailsPlugin.groovy
@@ -9,7 +9,7 @@ class CollectoryGrailsPlugin {
     def authenticateService
 
     // the plugin version
-    def version = "1.5"
+    def version = "1.6-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.3 > *"
     // resources that are excluded from plugin packaging
