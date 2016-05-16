b8da70cf9b91e4f6b94b4c1f21559dcb0028d9db
Peter Ansell <p_ansell@yahoo.com>
2016-05-16 10:43:20 +1000

    Bump to next snapshot for development

--- CollectoryGrailsPlugin.groovy
+++ CollectoryGrailsPlugin.groovy
@@ -9,7 +9,7 @@ class CollectoryGrailsPlugin {
     def authenticateService
 
     // the plugin version
-    def version = "1.5.9"
+    def version = "1.5.10-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.3 > *"
     // resources that are excluded from plugin packaging
