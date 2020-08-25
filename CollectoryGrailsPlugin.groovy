2ed9737c04db9a07fe9052d40ece43c4e5a2b207
Patricia Koh <patkyn@hotmail.com>
2020-08-26 07:51:25 +1000

    Set next snapshot version

--- CollectoryGrailsPlugin.groovy
+++ CollectoryGrailsPlugin.groovy
@@ -9,7 +9,7 @@ class CollectoryGrailsPlugin {
     def authenticateService
 
     // the plugin version
-    def version = "1.9.10"
+    def version = "1.9.11-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.5 > *"
     // resources that are excluded from plugin packaging
